61e8fd5d6812473853db21c3cce7975d43c2b676
Allison <allisonsouza10261@gmail.com>
2025-04-10 21:15:44 -0300

    Adding CommentsRepository and methods findAllByTask_Id, findAllByUser_Id, existsByUser_IdAndId

---
+++ src/main/java/com/br/tasksmanager/repositories/CommentsRepository.java
@@ -0,0 +1,15 @@
+package com.br.tasksmanager.repositories;
+
+import com.br.tasksmanager.models.Comments;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.UUID;
+
+@Repository
+public interface CommentsRepository extends JpaRepository<Comments, UUID> {
+    List<Comments> findAllByTask_Id(UUID taskId);
+    List<Comments> findAllByUser_Id(Long userId);
+    boolean existsByUser_IdAndId(Long userId, UUID id);
+}
